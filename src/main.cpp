de64b660d9da0d9aac2f81af3cf1447762b3d890
Rounak-Paul <rounakpaul@hotmail.com>
2026-03-02 23:24:54 +0530

    app config changed

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,14 @@ int main(int argc, char* argv[]) {
     
     sol::Application app;
     app.SetArgs(argc, argv);
-    app.Run("Sol", 600, 400);
+    tvk::AppConfig config;
+    config.title = "Sol";
+    config.width = 1280;
+    config.height = 720;
+    config.vsync = true;
+    config.enableIdleThrottling = true;
+    config.idleFrameRate = 30.0f;
+    app.Run(config);
     
     Logger::Info("Sol application terminated");
     return 0;
